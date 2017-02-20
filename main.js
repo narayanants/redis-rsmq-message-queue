181a5e22c733762b781c1af70359917ada52fe44
Narayanan Ts <tsnarayanan7@gmail.com>
2017-02-20 21:32:00 +0530

    added console.log statements

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var rsmq = new RedisSMQ( {host: "127.0.0.1", port: 6379, ns: "rsmq"} );
 
 
 rsmq.createQueue({qname:"myqueue"}, function (err, resp) {
+        console.log("Creating Queue");
         if (resp===1) {
             console.log("queue created");
         }
@@ -11,6 +12,7 @@ rsmq.createQueue({qname:"myqueue"}, function (err, resp) {
 
 // send a message
 rsmq.sendMessage({qname:"myqueue",message:"Hello World"},function(err,resp){
+    console.log("Sending message");
     if(resp){
         console.log("Message sent. ID:",resp);
     }
@@ -20,6 +22,7 @@ rsmq.sendMessage({qname:"myqueue",message:"Hello World"},function(err,resp){
 // Receive a message
 
 rsmq.receiveMessage({qname:'myqueue'},function(err,resp){
+    console.log("Receiving message");
     if(resp.id){
         console.log("Message received.",resp);
     }else{
@@ -30,6 +33,7 @@ rsmq.receiveMessage({qname:'myqueue'},function(err,resp){
 // Delete Message
 
 rsmq.deleteMessage({qname:"myqueue", id:"dhoiwpiirm15ce77305a5c3a3b0f230c6e20f09b55"}, function (err, resp) {
+    console.log("Deleting Message");
     if (resp===1) {
         console.log("Message deleted.") 
     }
